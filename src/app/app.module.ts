d32ee5f9c3da8a14b381de8ee7bd2ae3c39af1be
mathias73 <mat.micheli99@gmail.com>
2020-06-18 16:04:32 +0200

    adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ResumeComponent } from './resume/resume.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {H} from "@angular/cdk/keycodes";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ResumeComponent,
     ExperienceComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
     NoopAnimationsModule,
     NgbModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'resume',
+        component: ResumeComponent
+      },
+      {
+        path: 'experience',
+        component: ExperienceComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      },
+    ]),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
